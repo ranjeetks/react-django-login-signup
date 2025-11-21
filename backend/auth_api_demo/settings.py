4f62b39b65309be9726610292a8f53bd24924bc9
Ranjeet Kumar <rksingh.gzp@gmail.com>
2025-11-21 21:53:48 +0530

    chore: cleanup requirements for deployment

--- backend/auth_api_demo/settings.py
+++ backend/auth_api_demo/settings.py
@@ -159,9 +159,9 @@ REST_FRAMEWORK = {
     ],
 }
 # CORS settings
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Vite dev server
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",  # Vite dev server
+# ]
 # Allow all origins for development purposes
 CORS_ALLOW_CREDENTIALS = True
